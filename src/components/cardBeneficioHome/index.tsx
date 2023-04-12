795205239f1243c9e29250eb2ae5e9d39088700a
Mirosmar <mirosmarsantoss@gmail.com>
2023-04-12 15:17:08 -0300

    Fazendo modal de beneficio

--- src/components/cardBeneficioHome/index.tsx
+++ src/components/cardBeneficioHome/index.tsx
@@ -1,49 +1,99 @@
-import { Card, Text, Flex, Skeleton, Stack } from "@chakra-ui/react";
+import {
+  Text,
+  Button,
+  Card,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Skeleton,
+  Stack,
+  Box,
+} from "@chakra-ui/react";
 import Link from "next/link";
 import styles from "./styles.module.scss";
 import Image from "next/image";
+import { useDisclosure } from "@chakra-ui/react";
 
 import { IBeneficio } from "../../dtos/IBeneficio";
 
 export function CardBeneficio(IBeneficio: IBeneficio) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Link href={`/beneficio/${IBeneficio.id}`}>
-      <Card
-        border="2px"
-        rounded={"18"}
-        borderColor="#fff"
-        my={{ base: "2", md: "5", lg: "5", xl: "5" }}
-        mx={{ base: "1", md: "5", lg: "5", xl: "5" }}
-        width={{ base: "29vw", md: "35vw", lg: "35vw", xl: "23vw" }}
-        height={{ base: "19vh", md: "32", lg: "40", xl: "22vh" }}
-        opacity={0.5}
-        className={styles.card}
-      >
-        <Image
-          src={`${String(IBeneficio.urlImage)}`}
-          width={"300"}
-          height={"250"}
-          alt={String(IBeneficio.nome)}
-          className={styles.image}
-        ></Image>
-        <Flex
-          position={"absolute"}
-          color="#fff"
-          width={{ base: "full", md: "full", lg: "full", xl: "full" }}
-          height={{ base: "full", md: "full", lg: "full", xl: "full" }}
-          justifyContent="center"
-          alignItems="center"
+    <>
+      <Link href={""} onClick={onOpen}>
+        <Card
+          border="2px"
+          rounded={"18"}
+          borderColor="#fff"
+          my={{ base: "2", md: "5", lg: "5", xl: "5" }}
+          mx={{ base: "1", md: "5", lg: "5", xl: "5" }}
+          width={{ base: "29vw", md: "35vw", lg: "35vw", xl: "23vw" }}
+          height={{ base: "19vh", md: "32", lg: "40", xl: "22vh" }}
+          opacity={0.5}
+          className={styles.card}
         >
-          <Text
-            whiteSpace={"normal"}
-            textAlign={"center"}
-            fontSize={{ base: "initial", md: "3xl", lg: "4xl", xl: "4xl" }}
+          <Image
+            src={`${String(IBeneficio.urlImage)}`}
+            width={"300"}
+            height={"250"}
+            alt={String(IBeneficio.nome)}
+            className={styles.image}
+          ></Image>
+          <Flex
+            position={"absolute"}
+            color="#fff"
+            width={{ base: "full", md: "full", lg: "full", xl: "full" }}
+            height={{ base: "full", md: "full", lg: "full", xl: "full" }}
+            justifyContent="center"
+            alignItems="center"
           >
-            {IBeneficio.nome}
-          </Text>
-        </Flex>
-      </Card>
-    </Link>
+            <Text
+              whiteSpace={"normal"}
+              textAlign={"center"}
+              fontSize={{ base: "initial", md: "3xl", lg: "4xl", xl: "4xl" }}
+            >
+              {IBeneficio.nome}
+            </Text>
+          </Flex>
+        </Card>
+      </Link>
+
+      <Modal
+        isCentered
+        onClose={onClose}
+        isOpen={isOpen}
+        motionPreset="slideInBottom"
+        size={""}
+        scrollBehavior="outside"
+      >
+        <ModalOverlay />
+        <ModalContent
+          className={styles.modalContent}
+          height={{ base: "92vh", md: "90vh", lg: "100vh", xl: "100vh" }}
+          width={{ base: "100vw", md: "60vw", lg: "50vw", xl: "45vw" }}
+          textColor={"#fff"}
+          border={"none"}
+        >
+
+            <ModalHeader>{IBeneficio.nome}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text textAlign={"start"}>{IBeneficio.descricao}</Text>
+            </ModalBody>
+            <ModalFooter justifyContent={"center"}>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Fechar
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+      </Modal>
+    </>
   );
 }
 
