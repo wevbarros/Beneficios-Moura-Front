fd03a666b38a28d2963c5d340d39a26a7677347b
NckAlves <nicholasoxalves@gmail.com>
2023-05-09 12:07:32 -0300

    Responsividade categorias

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,9 +14,19 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="description" content="Página de Apresentação do Programa Para Você" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+        {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" /> */}
         <title>Para Você</title>
+
+        {/*TAGS PWA*/}
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+        <meta name="description" content="description of your project" />
+        <meta name="theme-color" content="#007DCE" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
       </Head>
+
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossOrigin="anonymous" />
       <ChakraProvider>
         <NavBar />
