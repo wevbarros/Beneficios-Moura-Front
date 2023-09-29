e7db24f83ebd5f3c61865409cb75dcbacec4ae3e
NckAlves <nicholasoxalves@gmail.com>
2023-09-29 16:12:28 -0300

    Delete Complete

--- src/components/edit_list/index.tsx
+++ src/components/edit_list/index.tsx
@@ -9,58 +9,73 @@ import { FormEditarBeneficio } from "../formEditarBeneficio";
 // import FormEditarBeneficio from "../formEditarBeneficio/index.tsx"
 
 export default function ListaEdicao() {
-  const { beneficios } = useBeneficiosController();
+    const [data, setData] = useState(null);
+    const { beneficios } = useBeneficiosController();
 
-  function deleteBeneficio(id: Number) {}
 
-  return (
-    <main className={`${styles.listaEdit}`}>
-      {beneficios ? (
-        <ul>
-          {beneficios.map((beneficio: IBeneficio) => (
-            <li key={Number(beneficio.id)} className={`${styles.listaItens}`}>
-              <img
-                className={`${styles.imgLista}`}
-                src={beneficio.urlImage}
-                alt=""
-              />
-              <div className={`${styles.tituloBeneficio}`}>
-                {" "}
-                {beneficio.nome}{" "}
-              </div>
-              <p className={`${styles.categorias}`}>
-                {(() => {
-                  switch (Number(beneficio.categoria)) {
-                    case 1:
-                      return "Estar Lado a Lado";
-                    case 2:
-                      return "Ficar Bem";
-                    case 3:
-                      return "Economizar";
-                    case 4:
-                      return "Ficar Saudável";
-                    default:
-                      return "Para Você";
-                  }
-                })()}
-              </p>
+    function deleteBeneficio(id: Number) {
+        const urlApi = 'https://apibeneficiosmoura.azurewebsites.net/beneficios';
+        axios
+            .delete(`${urlApi}/${id}`)
+            .then(() => {
+                alert("Benefício Removido");
 
-              <div className={`${styles.botoes}`}>
-                <FormEditarBeneficio {...beneficio} />
-                <p className={`${styles.trace}`}> | </p>
-                <button
-                  key={Number(beneficio.id)}
-                  onClick={() => deleteBeneficio(Number(beneficio.id))}
-                >
-                  <img src="../../images/icons/excluir.png" alt="" width={28} />
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Carregando Dados</p>
-      )}
-    </main>
-  );
+                const beneficios = data.filter(beneficio => beneficio.id !== id);
+                setData(beneficios)
+            })
+            .catch(error => {
+                console.error('Erro ao deletar o benefício:', error);
+            });
+    }
+
+    return (
+        <main className={`${styles.listaEdit}`}>
+            {beneficios ? (
+                <ul>
+                    {beneficios.map((beneficio: IBeneficio) => (
+                        <li key={Number(beneficio.id)} className={`${styles.listaItens}`}>
+                            <img
+                                className={`${styles.imgLista}`}
+                                src={beneficio.urlImage}
+                                alt=""
+                            />
+                            <div className={`${styles.tituloBeneficio}`}>
+                                {" "}
+                                {beneficio.nome}{" "}
+                            </div>
+                            <p className={`${styles.categorias}`}>
+                                {(() => {
+                                    switch (Number(beneficio.categoria)) {
+                                        case 1:
+                                            return "Estar Lado a Lado";
+                                        case 2:
+                                            return "Ficar Bem";
+                                        case 3:
+                                            return "Economizar";
+                                        case 4:
+                                            return "Ficar Saudável";
+                                        default:
+                                            return "Para Você";
+                                    }
+                                })()}
+                            </p>
+
+                            <div className={`${styles.botoes}`}>
+                                <FormEditarBeneficio {...beneficio} />
+                                <p className={`${styles.trace}`}> | </p>
+                                <button
+                                    key={Number(beneficio.id)}
+                                    onClick={() => deleteBeneficio(Number(beneficio.id))}
+                                >
+                                    <img src="../../images/icons/excluir.png" alt="" width={28} />
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Carregando Dados</p>
+            )}
+        </main>
+    );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   // baseURL: 'http//localhost:5218'
-  baseURL: 'http://localhost:5218/'
+  baseURL: 'https://apibeneficiosmoura.azurewebsites.net/'
 })
