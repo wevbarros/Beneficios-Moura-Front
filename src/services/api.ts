ae3c414e86d840e3148f68e1fe02ec76cf64126d
Weverton <88508196+wevbarros@users.noreply.github.com>
2023-11-20 13:47:37 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://apibeneficiosmoura.azurewebsites.net/'
+  baseURL: 'https://apibeneficiosmouradev.azurewebsites.net/'
 })
