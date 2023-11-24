73d8d2022204348a74fc378d0304e5c51d5ad2d2
Weverton Barros <weverton.barros@grupomoura.com>
2023-11-24 08:57:37 -0300

    change domain blob

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['beneficiosmourastorage.blob.core.windows.net'],
+    domains: ['mouratech.blob.core.windows.net'],
   },
   // webpack: (config, { isServer }) => {
   //   if (!isServer) {
